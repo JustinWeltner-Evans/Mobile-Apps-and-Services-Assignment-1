2294c5d10047afe6267de44aa28cb2a82be5d9bc
maryeprouty <meprouty@gatech.edu>
2019-01-26 13:22:01 -0500

    New activity for GameOver

---
+++ app/src/main/java/com/codepath/examples/basicsnakegame/GameOverActivity.java
@@ -0,0 +1,13 @@
+package com.codepath.examples.basicsnakegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameOverActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gameover);
+    }
+}
